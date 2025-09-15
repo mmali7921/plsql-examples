fa79a0fdb257309cfec4c1536e854f7c1080f422
Muhammed Ali <138666455+mmali7921@users.noreply.github.com>
2025-09-15 12:12:31 +0530

    Refactor SQL to improve formatting and output

--- check_odd_or_even.sql
+++ check_odd_or_even.sql
@@ -1,11 +1,9 @@
 DECLARE
     A NUMBER := &ENTER_NO;
-    
 BEGIN
-    IF MOD(A,2)=0 THEN
-    dbms_output.put_line('it is an even no.');
+    IF MOD(A, 2) = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('it is an even no.');
     ELSE
-    dbms_output.put_line('it is an odd no.');
+        DBMS_OUTPUT.PUT_LINE('it is an odd no.');
     END IF;
-END;    
-    
+END;
