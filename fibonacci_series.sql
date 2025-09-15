e6d17d882fe234e67708f5d311ac3894c5ca1f5a
Muhammed Ali <138666455+mmali7921@users.noreply.github.com>
2025-09-15 12:13:36 +0530

    Refactor Fibonacci series SQL script for clarity

--- fibonacci_series.sql
+++ fibonacci_series.sql
@@ -1,16 +1,17 @@
 DECLARE
-     N NUMBER:=&N;
+    N NUMBER := &N;
     A NUMBER := 0;
     B NUMBER := 1;
     C NUMBER;
 BEGIN
-   DBMS_OUTPUT.PUT_LINE('Fibonacci Series:');
-   DBMS_OUTPUT.PUT_LINE(A);
-   DBMS_OUTPUT.PUT_LINE(B);
-   FOR i IN 3..N LOOP
-   c:=a+b;
-   DBMS_OUTPUT.PUT_LINE(C);
-   a:=b;
-   b:=c;
-   END LOOP;
-END;   
+    DBMS_OUTPUT.PUT_LINE('Fibonacci Series:');
+    DBMS_OUTPUT.PUT_LINE(A);
+    DBMS_OUTPUT.PUT_LINE(B);
+    FOR I IN 3..N LOOP
+        C := A + B;
+        DBMS_OUTPUT.PUT_LINE(C);
+        A := B;
+        B := C;
+    END LOOP;
+
+END;
